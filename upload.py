c92452a5511e0b9b8d03acb8a017b12ce9d45c92
TeraWrist2692 <140892495+VIKIII2269@users.noreply.github.com>
2024-07-09 21:27:03 +0530

    Create upload.py

---
+++ upload.py
@@ -0,0 +1,15 @@
+@app.route('/upload', methods=['POST'])
+def upload_file():
+    if 'file' not in request.files:
+        return redirect(request.url)
+    file = request.files['file']
+    if file.filename == '':
+        return redirect(request.url)
+    if file:
+        filename = secure_filename(file.filename)
+        filepath = os.path.join(app.config['UPLOAD_FOLDER'], filename)
+        file.save(filepath)
+        global reference_image
+        reference_image = cv2.imread(filepath)
+        return redirect(url_for('index'))
+    return redirect(request.url)
